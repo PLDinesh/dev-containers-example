9f50adbaa49315b156f711a147797b2d71fe5a29
Dinesh Loganathan <dloganathan@ivp.in>
2024-11-03 14:22:03 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace dev_containers_example;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        
+        var app = builder.Build();
+        // Configure the HTTP request pipeline.
+
+          
+        //if (app.Environment.IsDevelopment())
+       // {
+            app.MapOpenApi();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/openapi/v1.json", "v1");
+            });
+       // }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
